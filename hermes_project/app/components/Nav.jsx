6db5c9c2c21fc8c7079a42f6488a4246d88805e2
Vesaun Shrestha <vesaunshrestha@Vesauns-MacBook-Air.local>
2025-03-02 05:36:46 -0700

    logo change

--- hermes_project/app/components/Nav.jsx
+++ hermes_project/app/components/Nav.jsx
@@ -18,11 +18,13 @@ export default function Navbar() {
         <Link href="/" className="logo">
 
         <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-black mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-            </svg>
-            <h1 className="text-4xl font-extrabold tracking-wider">HERMES</h1>
-        </div>        
+          <img 
+            src="/hermes_wings.jpg" 
+            alt="Hermes Logo" 
+            className="h-10 w-10 mr-4" 
+          />
+          <h1 className="text-4xl font-extrabold tracking-wider">HERMES</h1>
+        </div>      
         </Link>
 
       {/* Desktop Menu */}
